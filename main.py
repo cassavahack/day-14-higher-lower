c6ee08a795a4aa2e435b77a4d84f6cda1a5fe882
cassvahack <oise@elios.co>
2025-09-13 20:53:15 -0400

    mostly working higher lower project

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import random
 
 from game_data import data
 
-#print(logo)
-
 
 
 
@@ -15,14 +13,71 @@ def get_random_entry():
 
 A = get_random_entry()
 B = get_random_entry()
+score =0
 
-def display_choices(A, B):
+
+def display_choices(A,B):
+    print(logo)
+    print(A["follower_count"], B["follower_count"])
     print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}")
     print("vs")
     print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}")
+    print(f"Your score is {score}")
+
+
+
+def check_answer(A, B, player_guess):
+    if int(A["follower_count"]) > int(B["follower_count"]):
+        champion = A
+        champion_letter = "A"
+    else:
+        champion = B
+        champion_letter = "B"
+
+
+    if player_guess == champion_letter:
+        is_correct = True
+    else:
+        is_correct = False
+
+
+    return is_correct, champion
 
 
 
 
 
-display_choices(A, B)
+while True:
+    display_choices(A,B)
+    player_guess = input("Which one? “A” or “B”: ").upper()
+
+    is_correct, winner = check_answer(A, B, player_guess)
+
+    if is_correct:
+        score += 1
+        A = winner  # carry forward the winner
+        B = get_random_entry()
+
+    else:
+        print(f"Sorry, that’s wrong. Final score: {score}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
